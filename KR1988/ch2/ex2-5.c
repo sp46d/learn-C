d69eb87ba5a93ed7dabf8b6b25c0b055408c2d10
Sanghyuk Park <sanghyuk.park85@gmail.com>
2025-02-28 14:00:50 -0600

    Edit format

--- KR1988/ch2/ex2-5.c
+++ KR1988/ch2/ex2-5.c
@@ -1,6 +1,7 @@
-// Exercise 2-5. Write the function any(s1, s2), which returns the first location in a string s1
-// where any character from the string s2 occurs, or -1 if s1 contains no characters from s2.
-// (The standard library function strpbrk does the same job but returns a pointer to the location.)
+// Exercise 2-5. Write the function any(s1, s2), which returns the first
+// location in a string s1 where any character from the string s2 occurs, or -1
+// if s1 contains no characters from s2. (The standard library function strpbrk
+// does the same job but returns a pointer to the location.)
 
 #include <stdio.h>
 
@@ -9,7 +10,7 @@ int any(char s1[], char s2[]);
 
 int main(void)
 {
-    char *s1 = "Hello! my name is will! I am 39 years old.";
+    char* s1 = "Hello! my name is will! I am 39 years old.";
     char s2[100];
     int l;
 
@@ -17,8 +18,7 @@ int main(void)
     printf("Characters: ");
     scanf("%s", s2);
 
-    if ((l = any(s1, s2)) == -1)
-    {
+    if ((l = any(s1, s2)) == -1) {
         printf("None of the characters in '%s' found in the sentence.\n", s2);
         return 1;
     }
@@ -29,10 +29,8 @@ int main(void)
 int any(char s1[], char s2[])
 {
     int i, c;
-    for (i = 0; (c = s1[i]) != '\0'; i++)
-    {
-        for (int j = 0; s2[j] != '\0'; j++)
-        {
+    for (i = 0; (c = s1[i]) != '\0'; i++) {
+        for (int j = 0; s2[j] != '\0'; j++) {
             if (c == s2[j])
                 return i;
         }
