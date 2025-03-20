74efc851b2c292db3fff74979873c9484725b940
Sanghyuk Park <sanghyuk.park85@gmail.com>
2025-03-20 15:50:44 -0500

    Add my answer to Exercise 4-4, by adding various commands to ex4-3.c

--- KR1988/ch4/ex4-3.c
+++ KR1988/ch4/ex4-3.c
@@ -7,13 +7,6 @@
 #define NUMBER '0'
 #define BUFSIZE 5
 
-// function prototypes
-int getop(char[]);
-void push(double);
-double pop(void);
-int getch(void);
-void ungetch(int);
-
 // global variables
 // 1. stack
 int sp = 0; // next free stack position
@@ -23,10 +16,19 @@ double val[MAXVAL]; // value stack
 char buf[BUFSIZE]; // buffer for ungetch
 int bufp = 0; // next free position in buf
 
+// function prototypes
+int getop(char[]);
+void push(double);
+double pop(void);
+int getch(void);
+void ungetch(int);
+void clear_stack(void);
+
 int main(void)
 {
-    int type, op2_int, n;
-    double op2;
+    int type, n;
+    int command_entered = 0;
+    double op2, tmp, tmp1, tmp2;
     char s[MAXOP];
 
     while ((type = getop(s)) != EOF) {
@@ -53,15 +55,75 @@ int main(void)
             break;
         // Exercise 4-3: Add modulo operator
         case '%':
-            op2_int = pop();
-            if (op2_int != 0) {
+            op2 = pop();
+            if (op2 != 0) {
                 n = pop();
-                push(n - (int)(n / op2_int) * op2_int);
+                push(n - (int)(n / op2) * op2);
             } else
                 printf("error: modulus not allowed for zero divisor\n");
             break;
+        // Exercise 4-4: Print the top element of the stack without popping
+        case 'p':
+            if (sp < 1) {
+                printf("stack empty\n");
+                command_entered = 1;
+                break;
+            }
+            printf("The most top element is: %g\n", pop());
+            ++sp;
+            command_entered = 1;
+            break;
+        // Exercise 4-4: Duplicate the top element of the stack
+        case 'd':
+            if (sp < 1) {
+                printf("stack empty\n");
+                command_entered = 1;
+                break;
+            }
+            tmp = pop();
+            for (int i = 0; i < 2; i++)
+                push(tmp);
+            printf("Top element has been duplicated\n");
+            command_entered = 1;
+            break;
+        // Exercise 4-4: Swap the top two elements
+        case 's':
+            if (sp < 1) {
+                printf("stack empty\n");
+                command_entered = 1;
+                break;
+            }
+            tmp1 = pop();
+            tmp2 = pop();
+            push(tmp1);
+            push(tmp2);
+            printf("Top two elements have been swapped\n");
+            command_entered = 1;
+            break;
+        // Exercise 4-4: Clear the stack
+        case 'c':
+            sp = 0;
+            printf("Stack has been cleared\n");
+            command_entered = 1;
+            break;
+        // utility command for view: View all the values in the stack
+        case 'v':
+            if (sp < 1) {
+                printf("stack empty\n");
+                command_entered = 1;
+                break;
+            }
+            printf("Top: ");
+            for (int i = sp - 1; i > 0; --i)
+                printf("%g\n", val[i]);
+            printf("Bottom: %g\n", val[0]);
+            command_entered = 1;
+            break;
         case '\n':
-            printf(" -> %.8g\n", pop());
+            if (command_entered)
+                command_entered = 0;
+            else
+                printf(" -> %.8g\n", pop());
             break;
         default:
             printf("error: unknown command %s\n", s);
