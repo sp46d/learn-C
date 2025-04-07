5c5ceced5cf921908ca6d4b55a6be3a0b2e6ffb7
Sanghyuk Park <sanghyuk.park85@gmail.com>
2025-04-07 11:17:49 -0500

    Add print_array function

--- Klemens09/ch2/q2-4.c
+++ Klemens09/ch2/q2-4.c
@@ -2,12 +2,19 @@
 #include <stdio.h>
 #define ARR_SIZE 100
 
+void print_array(int in_array[], int array_size);
+
 int main(void)
 {
     int a[ARR_SIZE];
-    for (int i = 0; i < ARR_SIZE; ++i) {
+    for (int i = 0; i < ARR_SIZE; ++i)
         a[i] = pow(i + 1, 2);
-        printf("%d squared is %d\n", i + 1, a[i]);
-    }
+    print_array(a, ARR_SIZE);
     return 0;
+}
+
+void print_array(int in_array[], int array_size)
+{
+    for (int i = 0; i < array_size; ++i)
+        printf("%d squared is %d\n", i + 1, in_array[i]);
 }
