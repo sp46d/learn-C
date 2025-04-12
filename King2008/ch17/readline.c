2f7f9cbaca959ba9c0774060c006870014dd6c4e
Sanghyuk Park <sanghyuk.park85@gmail.com>
2025-04-12 15:28:46 -0500

    Add inventory program from ch 16.3

---
+++ King2008/ch17/readline.c
@@ -0,0 +1,20 @@
+#include "readline.h"
+#include <ctype.h>
+#include <stdio.h>
+
+int read_line(char str[], int n)
+{
+    int ch, i = 0;
+    while (isspace(ch = getchar()))
+        ;
+    while (ch != '\n' && ch != EOF) {
+        if (i < n) {
+            str[i] = ch;
+            i++;
+        }
+        ch = getchar();
+    }
+    str[i] = '\0';
+
+    return i;
+}
