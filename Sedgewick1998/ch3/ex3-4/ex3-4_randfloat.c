6615bdfa752c88fdc01c5024af8d06e8334cbd1b
Sanghyuk Park <sanghyuk.park85@gmail.com>
2025-05-08 14:24:09 -0500

    Add my answer to Exercise 3.4

---
+++ Sedgewick1998/ch3/ex3-4/ex3-4_randfloat.c
@@ -0,0 +1,36 @@
+#include "../ex3-3/randfloat.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    int rs[] = { 2, 4, 16 };
+    int Ns[] = { 1e3, 1e4, 1e5, 1e6 };
+    int r;
+    int N;
+    float m1;
+    float m2;
+    int x;
+
+    srand(time(NULL));
+    for (int i = 0; i < sizeof(rs) / sizeof(rs[0]); i++) {
+        r = rs[i];
+        for (int j = 0; j < sizeof(Ns) / sizeof(Ns[0]); j++) {
+            N = Ns[j];
+            m1 = 0.0;
+            m2 = 0.0;
+            for (int k = 0; k < N; k++) {
+                x = randfloat() * r;
+                m1 += (float)x / N;
+                m2 += ((float)(x * x)) / N;
+            }
+            printf(
+                "[Ex3-3: randfloat] r = %d, N = %d: Average = %f, Std. Dev. = "
+                "%g\n",
+                r, N, m1, sqrt(m2 - m1 * m1));
+        }
+    }
+    return 0;
+}
---
+++ Sedgewick1998/ch3/ex3-4/readme.md
@@ -0,0 +1,7 @@
+# Exercise 3.4
+
+Do Exercises 3.2 and 3.3 for `r = 2, 4, 16`.
+
+## My answer
+
+To do this exercise, I am going to reuse the programs that I wrote for Exercises 3.2 and 3.3 using `#include` statement. Since the programs that I am going to use are in different directories than the one where the client file resides, I am going to use relative path to include the programs. Thus, all we have to do is to update the client file, leaving the interface and implementation of the previous programs unchanged. The output is as below:
