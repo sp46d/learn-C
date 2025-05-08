045c2d5b734c62170348ec4952d2610f6af24c03
Sanghyuk Park <sanghyuk.park85@gmail.com>
2025-05-08 11:01:42 -0500

    Complete my answer to Exercise 3.1

--- Sedgewick1998/ch3/ex3-1/ex3-1.c
+++ Sedgewick1998/ch3/ex3-1/ex3-1.c
@@ -1,12 +1,3 @@
-# Exercise 3.1
-
-Find the largest and smallest numbers that you can represent with types `int`, `long int`, `short int`, `float`, and `double` in your programming environment.
-
-## My answer
-
-We can use `limits.h` and `float.h` standard libraries to find the limits of those types as follows:
-
-```C
 #include <float.h> // for FLT_MAX, FLT_MIN, DBL_MAX, DBL_MIN
 #include <limits.h> // for INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, SHRT_MAX, SHRT_MIN
 #include <stdio.h>
@@ -31,14 +22,3 @@ int main(void)
 
     return 0;
 }
-```
-
-And the output of the code above is as follows:
-
-```
-1. int (4 bytes): largest = 2147483647, smallest = -2147483648
-2. long int (8 bytes): largest = 9223372036854775807, smallest = -9223372036854775808
-3. short int (2 bytes): largest = 32767, smallest = -32768
-4. float (4 bytes): largest = 340282346638528859811704183484516925440.000000, smallest = 0.000000
-5. double (8 bytes): largest = 1.79769e+308, smallest = 2.22507e-308
-```
