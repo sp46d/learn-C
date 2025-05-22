b095de59a9e9b2bed9c80910cf9fc6313b81b7e3
Sanghyuk Park <sanghyuk.park85@gmail.com>
2025-05-22 10:58:38 -0500

    Add my answer to Exercise 4.3

---
+++ Sedgewick1998/ch4/ex4-3/ex4-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+// For int type
+typedef int Item;
+#define eq(A, B) (A == B)
+#define ITEMshow(ITEM) (printf("%d\n", ITEM))
+
+// For string type
+// typedef char* Item;
+// #define eq(A, B) (strcmp(A, B) == 0)
+// #define ITEMshow(ITEM) (printf("%s\n", ITEM))
