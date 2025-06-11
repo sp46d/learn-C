2246c1331e743d229c901c9d09e05c81a4d6f71e
Sanghyuk Park <sanghyuk.park85@gmail.com>
2025-06-11 11:33:19 -0500

    Add my answer to Exercise 4.26

---
+++ Sedgewick1998/ch4/ex4-26/uf.c
@@ -0,0 +1,42 @@
+#include "uf.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static int* id;
+static int* sz;
+
+void UFinit(int N)
+{
+    id = malloc(N * sizeof(*id));
+    sz = malloc(N * sizeof(*sz));
+    for (int i = 0; i < N; i++) {
+        id[i] = i;
+        sz[i] = 1;
+    }
+}
+
+static int find(int p)
+{
+    int i = p;
+    while (id[i] != i) {
+        i = id[id[i]];
+    }
+    return i;
+}
+
+int UFfind(int p, int q) { return find(p) == find(q); }
+void UFunion(int p, int q)
+{
+    int i = find(p);
+    int j = find(q);
+    if (i == j) {
+        return;
+    }
+    if (sz[i] > sz[j]) {
+        id[j] = i;
+        sz[j] += sz[i];
+    } else {
+        id[i] = j;
+        sz[i] += sz[j];
+    }
+}
